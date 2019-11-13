741b96d84a15e28fbe30ebf81916a238513c4a49
Giacomo Matthew Degl'Innocenti <info@giacomodeglinnocenti.com>
2019-11-13 12:40:40 +0100

    Optimization and switched to multiple language support

--- src/android/src/com/thomas/xunfeilistenspeaking/XunfeiListenSpeaking.java
+++ src/android/src/com/thomas/xunfeilistenspeaking/XunfeiListenSpeaking.java
@@ -30,9 +30,7 @@ public class XunfeiListenSpeaking extends CordovaPlugin{
     private Toast mToast;
     private Handler mHandler = new Handler();
 
-    private SpeechSynthesizer mTts;
     private SpeechRecognizer mIat;
-    private SharedPreferences mSharedPreferences;
     private String mEngineType = SpeechConstant.TYPE_CLOUD;
     private HashMap<String, String> mIatResults = new LinkedHashMap<String, String>();
 
@@ -60,6 +58,16 @@ public class XunfeiListenSpeaking extends CordovaPlugin{
 
         if (action.equals("startListen")){
 
+            String language;
+            try{
+                language = args.getString(0);
+                if ( language == null || language.isEmpty() ) {
+                    language = 'en_us';
+                }
+            }catch (Exception e){
+                language = 'en_us';
+            }
+
             String punc;
             try{
                 punc = args.getBoolean(1)?"1":"0";
@@ -69,7 +77,7 @@ public class XunfeiListenSpeaking extends CordovaPlugin{
 
             cordova.getThreadPool().execute(new Runnable() {
                 public void run() {
-                    startListenWidthNotDialog(punc);   
+                    startListen(language, punc);   
                 }
             });
 
@@ -92,7 +100,7 @@ public class XunfeiListenSpeaking extends CordovaPlugin{
         }
     }
 
-    private void startListenWidthNotDialog(String punc){
+    private void startListen(String language, String punc){
         mIat = SpeechRecognizer.createRecognizer(context, mInitListener);
 
         if (mIat.isListening()) {
@@ -101,7 +109,7 @@ public class XunfeiListenSpeaking extends CordovaPlugin{
 
         mIatResults.clear();
 
-        setParam(punc);
+        setParam(language, punc);
 
         this.ret = mIat.startListening(mRecognizerListener);
         if (this.ret != ErrorCode.SUCCESS) {
@@ -203,7 +211,7 @@ public class XunfeiListenSpeaking extends CordovaPlugin{
      * @param
      * @return
      */
-    public void setParam(String punc) {
+    public void setParam(String language, String punc) {
         // 清空参数
         mIat.setParameter(SpeechConstant.PARAMS, null);
 
@@ -212,33 +220,26 @@ public class XunfeiListenSpeaking extends CordovaPlugin{
         // 设置返回结果格式
         mIat.setParameter(SpeechConstant.RESULT_TYPE, "json");
 
-        String lag = mSharedPreferences.getString("iat_language_preference",
-                "mandarin");
-        if (lag.equals("en_us")) {
-            // 设置语言
-            mIat.setParameter(SpeechConstant.LANGUAGE, "en_us");
-        } else {
-            // 设置语言
-            mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
-            // 设置语言区域
-            mIat.setParameter(SpeechConstant.ACCENT, lag);
+        mIat.setParameter(SpeechConstant.LANGUAGE, language);
+        if ( language.equals("zh_cn") ) {
+            String chineseAccent = "mandarin";
+            mIat.setParameter(SpeechConstant.LANGUAGE, language);
+            mIat.setParameter(SpeechConstant.ACCENT, chineseAccent);
         }
 
         // 设置语音前端点:静音超时时间，即用户多长时间不说话则当做超时处理
-        mIat.setParameter(SpeechConstant.VAD_BOS, mSharedPreferences.getString("iat_vadbos_preference", "4000"));
+        mIat.setParameter(SpeechConstant.VAD_BOS, "4000");
 
         // 设置语音后端点:后端点静音检测时间，即用户停止说话多长时间内即认为不再输入， 自动停止录音
-        mIat.setParameter(SpeechConstant.VAD_EOS, mSharedPreferences.getString("iat_vadeos_preference", "1000"));
+        mIat.setParameter(SpeechConstant.VAD_EOS, "1000");
 
         // 设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
-        //mIat.setParameter(SpeechConstant.ASR_PTT, mSharedPreferences.getString("iat_punc_preference", "1"));
-        // mIat.setParameter(SpeechConstant.ASR_PTT, mSharedPreferences.getString("iat_punc_preference", punc));
         mIat.setParameter(SpeechConstant.ASR_PTT, punc);
 
         // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
         // 注：AUDIO_FORMAT参数语记需要更新版本才能生效
         mIat.setParameter(SpeechConstant.AUDIO_FORMAT, "wav");
-        mIat.setParameter(SpeechConstant.ASR_AUDIO_PATH, Environment.getExternalStorageDirectory() + "/msc/iat.wav");
+        mIat.setParameter(SpeechConstant.ASR_AUDIO_PATH, android.content.Context.getCacheDir() + "/msc/iat.wav");
     }
 
     private void showTip(final String str) {
